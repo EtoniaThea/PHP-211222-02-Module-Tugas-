5f1b6302bd9661da6b0f5b5caae5b3c122ff13af
EtoniaThea <waskithocito.a@gmail.com>
2022-12-21 20:34:19 +0700

    Tugas 1,2,3

---
+++ Tugas 1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 1</title>
+</head>
+<body>
+    <h2>Latihan 1</h2>
+    <?php 
+$angka=19;
+if($angka % 2 == 0)
+{
+    echo("Angka $angka bilangan genap");   
+}else{
+    echo("Angka $angka bilangan ganjil");   
+
+}
+?>
+
+</body>
+</html>
---
+++ Tugas 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+<?php 
+$i=-0;
+$var=0;
+
+while($var <= 12)
+{
+    $i += $var;
+    $var += 2;
+}
+echo("hasil penjumlahan bilangan genap 0 s.d. 6 adalah $i ")
+?>
+</body>
+</html>
